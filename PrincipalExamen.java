000edbdfef50ef92071dcf10551bf70c20ddcc8f
Boliver01 <97520533+Boliver01@users.noreply.github.com>
2022-01-18 18:26:00 +0100

    Update and rename README.md to PrincipalExamen.java

---
+++ PrincipalExamen.java
@@ -0,0 +1,9 @@
+# prueba
+public class PrincipalExamen {
+    
+    public static void main(String[] args) {
+        //SegundaLeyNewton.gravedad();
+        SegundaLeyNewton2.peso();
+    }
+    
+}
